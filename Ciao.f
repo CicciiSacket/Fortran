f5ca178dab88aef4fcbd0d8ee037c8025ac20b30
Francesco Sacco <cicciisacco@gmail.com>
2022-03-05 19:17:34 +0100

    First use of fortran with code by GINA

--- Ciao.f
+++ Ciao.f
@@ -2,4 +2,4 @@
 * L'ALFABETO DEL FORTRAN E' DA:  a-z A-Z 0-9  + - * / ( ) ' , . = : $ ^
 	WRITE(*,*) 'Benvenuti nel corso di Calcolo Numerico'
 	WRITE(*,*) 'Oggi e'' una bella giornata'
-	END		  
+	END
